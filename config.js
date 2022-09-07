ff9071d63e037ab7d306875bb48c02bd43703258
Saidmurodjon <83033317+Saidmurodjon@users.noreply.github.com>
2022-09-07 19:00:42 +0500

    DB up

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const { env } = process;
+
 module.exports = {
-    DB_HOST: env.DATABASE_HOST,
-    DB_USER: env.DATABASE_USER,
-    DB_PASSWORD: env.DATABASE_PASSWORD,
-    DB: env.DATABASE,
     PORT: env.PORT,
+    DB:env.DATABASE_URL
 };
--- data/DB.js
+++ data/DB.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql");
-const { DB_HOST, DB_USER, DB_PASSWORD, DB } = require("../config");
+const { DB } = require("../config");
+const {parseUri } = require('mysql-parse')
+const config=parseUri(DB)
 const db = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB,
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database,
 });
 module.exports = db;
