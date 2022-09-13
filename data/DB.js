ee94607ba08cac1d3cd194bad28bf3b94c5b192d
Saidmurodjon <83033317+Saidmurodjon@users.noreply.github.com>
2022-09-13 19:43:58 +0500

    g

--- data/DB.js
+++ data/DB.js
@@ -3,9 +3,9 @@ const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = require("../config");
 
 const db = mysql.createPool({
   connectionLimit: 100,
-  host:DB_HOST,
-  user: DB_USERNAME,
-  password:  DB_PASSWORD,
-  database: DB_NAME,
+  host:"domain-site.cg1t66reg2uh.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password:  "10202001",
+  database: "domain-site",
 });
 module.exports = db;
