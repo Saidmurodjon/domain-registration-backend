8b501086413d7d35d5e92d5325764a842f3af6d6
Saidmurodjon <83033317+Saidmurodjon@users.noreply.github.com>
2022-09-13 18:52:05 +0500

    up

--- data/MySQL.js
+++ data/MySQL.js
@@ -1,10 +1,8 @@
 module.exports = function () {
   const db = require("./DB");
-  db.getConnection((err,connection) => {
+  db.getConnection((err) => {
     if (err) {
-      connection.release();
       console.log(' Error getting mysql_pool connection: ' + err);
-      throw err;
     } else {
       console.log("MYSQL connected...");
     }
