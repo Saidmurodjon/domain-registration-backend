c97565fb042e2a0d763276e9ac987b661ff7c290
Saidmurodjon <83033317+Saidmurodjon@users.noreply.github.com>
2022-09-13 20:04:20 +0500

    domain-site

--- data/DB.js
+++ data/DB.js
@@ -1,6 +1,7 @@
 const mysql = require("mysql");
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = require("../config");
-
+const {DB, DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = require("../config");
+const {parseUri } = require('mysql-parse')
+console.log(parseUri(process.env.DATABASE_URL))
 const db = mysql.createPool({
   connectionLimit: 100,
   host:DB_HOST,
