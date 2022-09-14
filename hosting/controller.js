fb53512193d949bfe4fed0471d4760a164f93a29
Saidmurodjon <83033317+Saidmurodjon@users.noreply.github.com>
2022-09-14 15:40:17 +0500

    s add

--- hosting/controller.js
+++ hosting/controller.js
@@ -1,7 +1,7 @@
 const db = require("../data/DB");
 async function Get(req, res) {
   try {
-    db.query("SELECT * FROM hosting", async (error, results) => {
+    db.query("SELECT * FROM hostings", async (error, results) => {
       return res.status(200).send(results);
     });
   } catch (error) {
@@ -10,7 +10,7 @@ async function Get(req, res) {
 }
 async function Post(req, res) {
   try {
-    db.query("INSERT INTO hosting SET ?", req.body, async (error, results) => {
+    db.query("INSERT INTO hostings SET ?", req.body, async (error, results) => {
       return res.status(201).send(results);
     });
   } catch (error) {
@@ -21,14 +21,14 @@ async function Update(req, res) {
   const id = req.params.id;
   try {
     db.query(
-      `SELECT * FROM hosting WHERE id =${id}`,
+      `SELECT * FROM hostings WHERE id =${id}`,
       async (error, results) => {
         console.log(results);
         if (results.length <= 0) {
           return res.status(404).send(error);
         } else {
           db.query(
-            `UPDATE hosting SET ? WHERE id=${id}`,
+            `UPDATE hostings SET ? WHERE id=${id}`,
             req.body,
             async (error, results) => {
               if (error) {
@@ -48,14 +48,14 @@ async function Delete(req, res) {
   const id = req.params.id;
   try {
     db.query(
-      `SELECT * FROM hosting WHERE id =${id}`,
+      `SELECT * FROM hostings WHERE id =${id}`,
       async (error, results) => {
         console.log(results);
         if (results.length <= 0) {
           return res.status(404).send(error);
         } else {
           db.query(
-            `DELETE FROM hosting WHERE id=${id}`,
+            `DELETE FROM hostings WHERE id=${id}`,
             async (error, results) => {
               if (error) {
                 console.log(error);
